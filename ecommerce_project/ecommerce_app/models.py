9e717f60b8e14be67e47359bda32ae0192219851
Sanjeev Kumar <sk330511@gmail.com>
2023-12-11 19:11:42 +0530

    change models to use django abstract base classes

--- ecommerce_project/ecommerce_app/models.py
+++ ecommerce_project/ecommerce_app/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Category(models.Model):
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Category(BaseModel):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
-class Product(models.Model):
+class Product(BaseModel):
     name = models.CharField(max_length=255)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -16,10 +23,16 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-class Order(models.Model):
+class Order(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
 
+class Review(BaseModel):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
 
+    def __str__(self):
+        return f"{self.user.username} - {self.product.name} - {self.created_at}"
